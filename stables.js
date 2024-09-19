2fa0b41e62431d56bd121cd9aa6fcbc588aae568
Sonja Farrell <sonjafarrell@gmail.com>
2024-09-19 15:43:05 -0700

    initialize stables and add a horse

---
+++ stables.js
@@ -0,0 +1,11 @@
+console.log("horses are great!")
+let horseName = "Coolguy"; // this is a string
+horseName = "Charlie"; // for strings, prefer double quotes
+let horseAge = 5;
+console.log(horseName);
+console.log(horseAge);
+let isHorseInStable = false;
+console.log(isHorseInStable);
+console.log(horseName);
+horseName = "Coolhorse"
+console.log(horseName);
