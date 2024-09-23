6281b6deaa323bfda592af219bbfe559f2012a7c
Sonja Farrell <sonjafarrell@gmail.com>
2024-09-23 00:27:30 -0700

    Update problem-file.js

--- problem-file.js
+++ problem-file.js
@@ -6,7 +6,7 @@ The quick brown fox jumps over the lazy spaghetti.
 
 The quick brown fox jumps over the lazy lasagna. Yuck!
 
-The quick brown fox doesn't jump over the lazy dog. It just stands there and does nothing.
+The quick brown fox doesn't jump over the lazy dog. It just stands there and barks.
 
 Let's also add our new terminal commands we learned!
 
@@ -14,4 +14,4 @@ Let's also add our new terminal commands we learned!
 
 you can copy the path to your current location in the directory by using the contextual menu and "get info" on MacOS.
 
-
+Hey there. We're about to create a conflict! But not with this line, this line is just to show you we can add multiple things to a file and most of the time, it will be okay.
