43e51e2a3e99d97f4fd99cb85c8d24aca4e3bbe0
Sonja Farrell <sonjafarrell@gmail.com>
2024-09-26 14:37:21 -0700

    add operators to my stable

--- stables.js
+++ stables.js
@@ -2,12 +2,15 @@ let horseName = "Coolguy";
 console.log(horseName);
 horseName = "Charlie";
 console.log(horseName);
-let horseAge = 8968767865;
+let horseAge = 5;
 console.log(horseAge);
 let isHorseInside = true;
 console.log(isHorseInside);
 
-
-
-
-
+const STABLE_MONTHLY_FEE = 100;
+let horseIntroduction = " is the name of my horse";
+// let rentIntroduction = "it costs " + STABLE_MONTHLY_FEE + " to board " + horseName;
+let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName}`;
+// let stableIntroduction = "Welcome to " + horseName + "'s stable 🐴";
+let stableIntroduction = `Welcome to "${horseName}'s" stable`;
+console.log(stableIntroduction);
