3a8648cbd86db3a948a22ce417da97ebfb858881
Sonja Farrell <sonjafarrell@gmail.com>
2024-09-27 15:24:36 -0700

    adding info to stables

--- stables.js
+++ stables.js
@@ -15,13 +15,12 @@ const STABLE_MONTHLY_FEE = 117; // uppercase is preferred for constants
 console.log(STABLE_MONTHLY_FEE);
 
 console.log("The stable monthly fee is " + STABLE_MONTHLY_FEE); // concatenation
-console.log("My horse's name is " + horseName + " and he is " + (horseAge - 3) + " years old");
-console.log(`My horse's name is "${horseName}" and costs ${STABLE_MONTHLY_FEE} to board him`);
+console.log("My horse's name is " + horseName + "and he is " + (horseAge - 3) + " years old");
+console.log(`My horse's name is ${horseName} and costs ${STABLE_MONTHLY_FEE} to board him`);
 console.log("My horse is grey");
 console.log(STABLE_MONTHLY_FEE - 20); 
 console.log(false + 2); // type coercion
 console.log(false + "NaN");
-// spellcheck plugin <---- try it out!
 
 
 
