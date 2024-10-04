f2878ff67cd94a28d2eef6b1150efaf289b590da
Sonja Farrell <sonjafarrell@gmail.com>
2024-10-03 19:16:15 -0700

    add lesson 5 code and invite visitors to the stable

--- stables.js
+++ stables.js
@@ -1,11 +1,29 @@
 // LESSON-3 Code
 let horseName = "Coolguy"; 
 horseName = "Charlie";
-let horseAge = 5;
-let isHorseInside = true;
+let horseNickname = "Chuck";
+let horseAge = 1;
+let isHorseInside = false;
 
 // LESSON-4 Code
 const STABLE_MONTHLY_FEE = 117; // uppercase is preferred for constants
 let stableIntroduction = `Welcome to "${horseName}'s" stable`;
 console.log(stableIntroduction);
 console.log(`My horse's name is ${horseName} and it costs ${STABLE_MONTHLY_FEE} to board them!`);
+
+// LESSON-5 Code
+if(isHorseInside) {
+    console.log(horseName + " is inside!")
+} else {
+    console.log(horseName + " is outside!");
+}
+
+let visitingHorseName = "🍓 Strawberry";
+
+if ((horseName === "Charlie" && isHorseInside) && (visitingHorseName === "🍓 Strawberry" || visitingHorseName === "🥬 Lettuce")) {
+    console.log(`${horseName} has a visitor and needs to come outside to see ${visitingHorseName}`);
+} else if ((horseName === "Charlie" && !isHorseInside) && (visitingHorseName === "🍓 Strawberry" || visitingHorseName === "🥬 Lettuce")) {
+    console.log(horseName + " is outside with " + visitingHorseName);
+} else {
+    console.log(horseName + " needs to figure what is going on");
+}
