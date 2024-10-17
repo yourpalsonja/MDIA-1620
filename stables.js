1c8519e863157e5229deadb6fd92213e86faa2e4
Sonja Farrell <sonjafarrell@gmail.com>
2024-10-17 00:18:42 -0700

    update stables with lesson 6 content

--- stables.js
+++ stables.js
@@ -1,5 +1,5 @@
 // LESSON-3 Code
-let horseName = "Coolguy"; 
+let horseName = "Coolguy";
 horseName = "Charlie";
 let horseNickname = "Chuck";
 let horseAge = 1;
@@ -9,21 +9,33 @@ let isHorseInside = false;
 const STABLE_MONTHLY_FEE = 117; // uppercase is preferred for constants
 let stableIntroduction = `Welcome to "${horseName}'s" stable`;
 console.log(stableIntroduction);
-console.log(`My horse's name is ${horseName} and it costs ${STABLE_MONTHLY_FEE} to board them!`);
+console.log(
+    `My horse's name is ${horseName} and it costs ${STABLE_MONTHLY_FEE} to board them!`
+);
 
 // LESSON-5 Code
-if(isHorseInside) {
-    document.write(horseName + " is inside!")
+if (isHorseInside) {
+    console.log(horseName + " is inside!");
 } else {
-    document.write(horseName + " is outside!");
+    console.log(horseName + " is outside!");
 }
 
-// let visitingHorseName = "🍓 Strawberry";
+// LESSON 6 CODE
+// Horse = [Charlie, Strawberry, Beans]
+let horseNames = [horseName, "Strawberry", "Beans"];
+let horseAges = [horseAge, 2, 10];
+let horseLocations = [isHorseInside, false, true];
+console.log(`Welcome to my stables! There are ${horseNames.length} horses staying here! Their names are: ${horseNames[0]}, ${horseNames[1]}, ${horseNames[2]}`);
+console.log("Welcome to my stables! There are " + horseNames.length + " horses staying here! Their names are " + horseNames[0] + ", " + horseNames[1] + ", " + horseNames[2] + "!");
+if(horseLocations[2]){
+    console.log(horseNames[2] + " is outside!");
+}
+console.log(horseAges[2] + horseAges[1]); //12
 
-// if ((horseName === "Charlie" && isHorseInside) && (visitingHorseName === "🍓 Strawberry" || visitingHorseName === "🥬 Lettuce")) {
-//     console.log(`${horseName} has a visitor and needs to come outside to see ${visitingHorseName}`);
-// } else if ((horseName === "Charlie" && !isHorseInside) && (visitingHorseName === "🍓 Strawberry" || visitingHorseName === "🥬 Lettuce")) {
-//     console.log(horseName + " is outside with " + visitingHorseName);
-// } else {
-//     console.log(horseName + " needs to figure what is going on");
-// }
+if(horseNames[0] === "Charlie") {
+    console.log("Hi Charlie!")
+}
+
+if(horseNames.length < 5) {
+ console.log("These stables are unpopular");
+}
