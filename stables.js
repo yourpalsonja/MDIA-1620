848a9d3c5ae2abcc3baeb82cb317453125c9a8e5
Sonja Farrell <sonjafarrell@gmail.com>
2024-11-13 13:56:59 -0800

    add objects to stables

--- stables.js
+++ stables.js
@@ -1,41 +1,44 @@
-// LESSON-3 Code
-let horseName = "Coolguy";
-horseName = "Charlie";
-let horseNickname = "Chuck";
-let horseAge = 1;
-let isHorseInside = false;
-
-// LESSON-4 Code
-const STABLE_MONTHLY_FEE = 117; // uppercase is preferred for constants
-let stableIntroduction = `Welcome to "${horseName}'s" stable`;
-console.log(stableIntroduction);
-console.log(
-    `My horse's name is ${horseName} and it costs ${STABLE_MONTHLY_FEE} to board them!`
-);
-
-// LESSON-5 Code
-if (isHorseInside) {
-    console.log(horseName + " is inside!");
-} else {
-    console.log(horseName + " is outside!");
+function Horse(name, nickname, age, faveTreat, isInside) {
+    this.name = name;
+    this.nickname = nickname;
+    this.age = age;
+    this.faveTreat = faveTreat;
+    this.isInside = isInside;
+    this.rent = 125;
+    this.intro = function () {
+        console.log(
+            `My horse is called ${this.name} and they are ${this.age} years old!`
+        );
+    };
 }
 
-// LESSON 6 CODE
-// Horse = [Charlie, Strawberry, Beans]
-let horseNames = [horseName, "Strawberry", "Beans"];
-let horseAges = [horseAge, 2, 10];
-let horseLocations = [isHorseInside, false, true];
-console.log(`Welcome to my stables! There are ${horseNames.length} horses staying here! Their names are: ${horseNames[0]}, ${horseNames[1]}, ${horseNames[2]}`);
-console.log("Welcome to my stables! There are " + horseNames.length + " horses staying here! Their names are " + horseNames[0] + ", " + horseNames[1] + ", " + horseNames[2] + "!");
-if(horseLocations[2]){
-    console.log(horseNames[2] + " is outside!");
-}
-console.log(horseAges[2] + horseAges[1]); //12
+const strawberry = new Horse("Strawberry", "Shortcake", 3, "carrots", true);
+const beans = new Horse("Beans", "Beanie", 5, "beans", false);
+const charlie = new Horse("Charlie", "Chuck", 13, "raspberries", true);
 
-if(horseNames[0] === "Charlie") {
-    console.log("Hi Charlie!")
+console.log(strawberry);
+console.log(beans);
+console.log(charlie);
+console.log(typeof charlie);
+
+var numbers = [10, 20, 30, 40, 50];
+for (var i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
 }
 
-if(horseNames.length < 5) {
- console.log("These stables are unpopular");
-}
+// let stableIntroduction = `Welcome to "${horseName}'s" stable`;
+// let horseIntro = `There are ${horseNames.length} horses staying at my stables: ${horseNames[0]}, ${horseNames[1]}, ${horseNames[2]}!`;
+
+// function getHorseLocation(horse) {
+//     if (areHorsesInside[horse]) {
+//         return horseNames[horse] + " is inside.";
+//     }
+
+//     return horseNames[horse] + " is outside";
+// }
+
+// function getHorseNicknames(horse, friend) {
+//     let string = `${horseNames[horse]}'s nickname is ${horseNicknames[horse]}, and they have a friend named ${horseNames[friend]}, who is nicknamed ${horseNicknames[friend]}!`;
+
+//     return string;
+// }
