39f19b70e7a6ba13a54617db3c0a0e0fef9466c7
yourpalsonja <sonjafarrell@gmail.com>
2025-10-09 13:13:56 -0700

    add initial lab document

---
+++ stables.js
@@ -0,0 +1,85 @@
+// Welcome!
+//
+//                                                  #    #
+//                                             %%% ##   ##
+//                                          %%%%% ###%🎀###
+//                                         %%%%% ### %%% #
+//                                       %%%%%% ### %%% ###
+//                                        %%%% ## %% #######
+//                                       %%%%% # %% #@#####
+//                                     %%%%%% # % #########
+//                                    %%%%% ##### #########
+//                          ###        %% ####### #########
+//                 %%% ############    ########### ########
+//              %%%% ############################### #######
+//            %%%%% ################################## ######
+//          %%%%%% #################################### #C###
+//         %%%🌸%% #####################################  ###
+//         %%%%% #######################################
+//        %%%%%% ############### BEANS ##################
+//     % %%%%%%% ############### BEANS ##################
+//      %%%%%%%%% ############## BEANS ##################
+//     %%%%%%%%%% ########################################
+//  %%% %%%%%%%%   ###### ################################
+//    %%%%%%%%      ###### #################### ##########
+// % %%%🌸%%%%        ####### ########### ###### ##########
+//  %%%%%%%%%         #######  ########### ###### ########
+// %%%%%%%%%%          ##### ###  ######### ####### ######
+//  %%%%%%%%%%          #### ##               ####### ####
+//  %🌸%%%%%%%%%           ## #                  ##### ###
+//   %%  %% % %%         # ##                      ## ###
+//     %   %    %        # ###                      # ###
+//                        # ###                     ## ###
+//                        # ###                     ## ###
+//                        # ####                   #### ##
+//                       ### ###                  ##### ###
+//                      ####  ###                 ####   ##
+//                     #####   ###                 ##    ##
+//                    #####    ####                      ###
+//                     ##        ###                     ###
+//                                ####                     ##
+//                                 ####                    ###
+//                                                         ####
+//                                                          ##
+//
+// This is a template for your labs and final project. You'll be setting up a stable and making
+// sure it runs smoothly.
+//
+// Please do not create a new file for each lab!
+// Feel free to keep the headings (e.g.: "Variables"), but please delete any boilerplate
+// comments. (Your own comments, where necessary, are acceptable.)
+//
+// NOTE: UNLESS SPECIFIED, VARIABLE NAMING IS UP TO YOU. THERE SHOULD BE NO HARD-CODED
+// NUMBERS OR STRINGS WHERE VARIBALES WOULD BE PREFERRED.
+
+//------------------------- Seting up shop (Lab #1a, Week 3) -------------------------//
+
+// Declare variables with values for the following:
+// - the name of the horse at the stable
+// - the age of the horse
+// - whether the horse is inside or outside
+// - the cost to board the horse monthly
+// - the fee for a late payment (monthly rate + 20%)
+//
+// Create a variable and use it to store a message for visitors to the stable.
+// Create a variable and use it to store a message that monthly payment is late, and the amount owing.
+// Include the name of your horse in the message.
+
+//------------------------- First day (Lab #1b, Week 4) -------------------------//
+
+// Using an object, add at least 3 horses to your stables.
+//
+// The horses should have the following properties:
+// - name, nickname, favorite treat (string)
+// - age, monthly rent (number)
+// - location (boolean inside/outside)
+// - two unique properties of your choice (use any primitive)
+
+// Store the horses you've just created in a "horses" variable.
+// Keep your old horse info from week one for now.
+
+// Fancy! Another horse wants to be stabled! Create a variable that stores an object
+// literal of your new horse, and add it to your "horses" variable.
+
+// Initialize new property to your horses: a boolean that allows you to check if your horse
+// is hungry or not. Use dot notation.
